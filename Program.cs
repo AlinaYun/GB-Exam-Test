a47a62ec79c769b1f0a4e52af65e9ebda83e21d5
AYun <alina.yun@ya.ru>
2022-05-09 16:32:08 +0300

    Изменен метод формирования массива из строк, длина которых меньше, либо равна 3 символа - удалено создание третьего массива

--- Program.cs
+++ Program.cs
@@ -1,37 +1,16 @@
 ﻿// Написать программу, которая из имеющегося массива строк формируем массив из строк, длина которых меньше, либо равна 3 символа.
 
 
-// Метод для заполнения массива с клавиатуры
+// Метод для создания и заполнения массива с клавиатуры
 string[] GenerateStringArray(int numberOfElements)    // numberOfElements - количество элементов в массиве
 {
     string[] stringArray = new string[numberOfElements];  //создаем новый пустой массив
     for (int i = 0; i < numberOfElements; i++)
     {
         Console.WriteLine($"Введите {i + 1} элемент в массиве:");
-        stringArray[i] = Console.ReadLine();              // присваиваем значение каждому элементу массива
+        stringArray[i] = Console.ReadLine();              // ввод с клавиатуры каждого элемента
     }
     return stringArray;
-}
-
-// Метод для формирования массива из строк, длина которых меньше, либо равна 3 символа
-string[] GetModifiedArray(string[] givenArray)
-{
-    int counter = 0;           // счетчик для определения количества элементов с длиной <= 3 символа
-    string[] modifiedArray = new string[givenArray.Length];  // создаем новый массив, длина которого равна счетчику
-    for (int i = 0; i < givenArray.Length; i++)
-    {
-        if (givenArray[i].Length <= 3)
-        {
-            modifiedArray[counter] = givenArray[i];  // если длина элемента во входящем массиве меньше, либо равна 3, добавляем этот элемент в новый массив
-            counter += 1;
-        }
-    }
-    string[] realSizeArray = new string[counter]; // создаем новый массив с длиной = кол-ву подходящих элементов
-    for (int j = 0; j < counter; j++)
-    {
-        realSizeArray[j] = modifiedArray[j];      
-    }
-    return realSizeArray;
 }
 
 // Метод для вывода массива
@@ -52,6 +31,32 @@ void PrintStringArray(string[] stringArray)
     Console.WriteLine("]");
     Console.WriteLine();
 }
+
+// Метод для формирования массива из строк, длина которых меньше, либо равна 3 символа
+string[] GetModifiedArray(string[] givenArray)
+{
+    int len = givenArray.Length;
+    int counter = 0;           // счетчик для определения количества элементов с длиной <= 3 символа
+    for (int i = 0; i < len; i++)
+    {
+        if (givenArray[i].Length <= 3)
+        {
+            counter += 1;
+        }
+    }
+    string[] modifiedArray = new string[counter];  // создаем новый массив, длина которого равна счетчику
+    int j = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (givenArray[i].Length <= 3)
+        {
+            modifiedArray[j] = givenArray[i];  // если длина элемента во входящем массиве меньше, либо равна 3, добавляем этот элемент в новый массив
+            j += 1;
+        }
+    }
+    return modifiedArray;
+}
+
 
 Console.WriteLine("Введите количество элементов в массиве:");
 int numberOfElements = Convert.ToInt32(Console.ReadLine());
