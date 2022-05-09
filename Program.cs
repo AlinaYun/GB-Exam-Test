9dbdc2c8c4443ace1e3f427fdfe0fd3b37621964
AYun <alina.yun@ya.ru>
2022-05-09 03:03:52 +0300

    Изменен метод формирования массива из строк, длина которых меньше, либо равна 3 символа - удален LINQ

--- Program.cs
+++ Program.cs
@@ -16,16 +16,22 @@ string[] GenerateStringArray(int numberOfElements)    // numberOfElements - ко
 // Метод для формирования массива из строк, длина которых меньше, либо равна 3 символа
 string[] GetModifiedArray(string[] givenArray)
 {
-    string[] modifiedArray = new string[givenArray.Length];
+    int counter = 0;           // счетчик для определения количества элементов с длиной <= 3 символа
+    string[] modifiedArray = new string[givenArray.Length];  // создаем новый массив, длина которого равна счетчику
     for (int i = 0; i < givenArray.Length; i++)
     {
         if (givenArray[i].Length <= 3)
         {
-            modifiedArray[i] = givenArray[i];
+            modifiedArray[counter] = givenArray[i];  // если длина элемента во входящем массиве меньше, либо равна 3, добавляем этот элемент в новый массив
+            counter += 1;
         }
     }
-    modifiedArray = modifiedArray.Where(x => x != null).ToArray();
-    return modifiedArray;
+    string[] realSizeArray = new string[counter]; // создаем новый массив с длиной = кол-ву подходящих элементов
+    for (int j = 0; j < counter; j++)
+    {
+        realSizeArray[j] = modifiedArray[j];      
+    }
+    return realSizeArray;
 }
 
 // Метод для вывода массива
